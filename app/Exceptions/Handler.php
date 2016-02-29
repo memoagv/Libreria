abc40023c1ad1ad33e00a2e2388f72bbc64d195e
vagrant <vagrant@homestead>
2016-02-29 23:22:58 +0000

    corrigiendo el error en las rutas

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,7 +45,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $e)
     {
-       
-        return parent::render($request, $e);
+       if ($e-> getStatusCode()==404) {
+         return response("Ocurrio un error");
+        
+        }
+       // return parent::render($request, $e);
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,9 +1,18 @@
 <?php
 Route::get('/', function () {
-	//return view('home'); 
-	return "usando git";
+	return view('home'); 
+	//return "usando git";
 });
 
 Route::POST('/form','ambientesController@show');
 //controladores
 Route::get('Controller','ambientesController@index');
+
+/*
+Route::get('/nombre/{nombre}', function ($nombre) {
+    return "pagina disponible ";
+})->where('nombre', '[A-Za-z]+');
+
+*/
+
+
